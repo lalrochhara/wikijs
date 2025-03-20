903c72f15c6c4cbc4094bdbf016e959b45772bb8
Nicky Lalrochhara <nickylalrochhara917@gmail.com>
2025-03-20 11:14:57 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,12 +4,12 @@ echo "============================="
 echo "WIKI.JS - HEROKU INSTALLATION"
 echo "============================="
 
-VERSION=$(curl -L -s -S https://raw.githubusercontent.com/Requarks/wiki-v1/master/CURRENT)
+VERSION=$(curl -L -s -S https://raw.githubusercontent.com/lalrochhara/wiki-v1/master/CURRENT)
 
 echo "-> Fetching latest build..."
-curl -L -s -S https://github.com/Requarks/wiki-v1/releases/download/v$VERSION/wiki-js.tar.gz | tar xz -C ./wiki
+curl -L -s -S https://github.com/lalrochhara/wiki-v1/releases/download/v$VERSION/wiki-js.tar.gz | tar xz -C ./wiki
 
 echo "-> Fetching dependencies..."
-curl -L -s -S https://github.com/Requarks/wiki-v1/releases/download/v$VERSION/node_modules.tar.gz | tar xz -C ./wiki
+curl -L -s -S https://github.com/lalrochhara/wiki-v1/releases/download/v$VERSION/node_modules.tar.gz | tar xz -C ./wiki
 
 echo "-> Installation Complete"
